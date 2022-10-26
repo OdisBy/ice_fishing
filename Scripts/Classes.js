ef3daf455b8126c9701402341cc563883c1df83a
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2022-10-26 06:04:00 -0300

    meudues

--- Scripts/Classes.js
+++ Scripts/Classes.js
@@ -1,179 +1,427 @@
-class Obj{
-    frame = 1;
-    timer = 0;
-    pescado = false;
-    coletado = false;
+class Obj {
+  frame = 1
+  timer = 0
+  pescado = false
+  coletado = false
 
-    tiposPossiveisObjeto = ["Peixes", "Lixos", "Inimigos", "Minhocas"];
-    peixesPossiveis = ["Salmao", "Tilapia", "Pacu"]
-    lixosPossiveis = ["Bota"]
-    inimigosPossiveis = ["Tubarao", "Caranguejo"]
-    minhocasPossiveis = ["Lata"]
+  tiposPossiveisObjeto = ['Peixes', 'Lixos', 'Inimigos', 'Minhocas']
+  peixesPossiveis = ['Salmao', 'Tilapia', 'Pacu']
+  lixosPossiveis = ['Bota']
+  inimigosPossiveis = ['Tubarao', 'Caranguejo']
+  minhocasPossiveis = ['Lata']
 
-    constructor(x, y, width, height, velocidade, image){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.velocidade = velocidade;
-        this.image = image;
+  constructor(x, y, width, height, velocidade, image) {
+    this.x = x
+    this.y = y
+    this.width = width
+    this.height = height
+    this.velocidade = velocidade
+    this.image = image
+  }
+  desenha() {
+    var img = new Image()
+    img.src = this.image
+    pincel.drawImage(img, this.x, this.y, this.width, this.height)
+  }
+
+  animation(name) {
+    this.timer += 1
+    if (this.timer > 10) {
+      this.timer = 0
+      this.frame += 1
     }
-    desenha(){
-        var img = new Image();
-        img.src = this.image;
-        pincel.drawImage(img, this.x, this.y, this.width, this.height);
+    if (this.frame > 4) {
+      this.frame = 1
     }
+    this.image = 'Assets/' + name + this.frame + '.png'
+  }
 
-    animation(name){
-        this.timer += 1;
-        if(this.timer > 10){
-            this.timer = 0;
-            this.frame += 1;
-        }
-        if(this.frame > 4){
-            this.frame = 1;
-        }
-        this.image = "Assets/" + name + this.frame + ".png";
-    }
+  movement() {
+    this.x += this.velocidade
+  }
 
-    movement(){
-        this.x += this.velocidade;
-    }
-
-    peixePescado(x, y, imagemPescado){
-        var img = new Image();
-        img.src = imagemPescado
-        pincel.drawImage(img, x, y+20, this.height, this.width);
-    }
-
+  peixePescado(x, y, imagemPescado) {
+    var img = new Image()
+    img.src = imagemPescado
+    pincel.drawImage(img, x, y + 20, this.height, this.width)
+  }
 }
 
-class Img{
-    constructor(x, y, width, height, image){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.image = image;
-    }
-    desenha(){
-        var img = new Image();
-        img.src = this.image;
-        pincel.drawImage(img, this.x, this.y, this.width, this.height);
-    }
+class Img {
+  constructor(x, y, width, height, image) {
+    this.x = x
+    this.y = y
+    this.width = width
+    this.height = height
+    this.image = image
+  }
+  desenha() {
+    var img = new Image()
+    img.src = this.image
+    pincel.drawImage(img, this.x, this.y, this.width, this.height)
+  }
 }
 
-class Player extends Obj{
-    lifes = 3
-    peixesPescados = 0
-    timer = 300
-    iscas = 3
+class Button extends Img {
+  clickButton(x, y) {
+    if (
+      x > this.x &&
+      x < this.x + this.width &&
+      y > this.y &&
+      y < this.y + this.height
+    ) {
+      return true
+    }
+  }
 }
 
-class LinhaVaraPesca extends Obj{
-    velocidade = 1
-    // image = "Assets/linha.png"
+class Player extends Obj {
+  lifes = 3
+  peixesPescados = 0
+  timer = 300
+  iscas = 3
+  tempoTubarao = 5
 
-    collide(obj){   
-        if(this.x < obj.x + obj.width &&
-            this.x + obj.width > obj.x &&
-            this.y < obj.y + obj.height &&
-            this.y + obj.height > obj.y
-            ){
-                return true;
-            }else{
-                return false;
+  ret = ''
+
+  fancyTimeFormat(duration) {
+    // Hours, minutes and seconds
+    var hrs = ~~(duration / 3600)
+    var mins = ~~((duration % 3600) / 60)
+    var secs = ~~duration % 60
+
+    this.ret = ''
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+
+    if (hrs > 0) {
+      this.ret += '' + hrs + ':' + (mins < 10 ? '0' : '')
+    }
+
+    this.ret += '' + mins + ':' + (secs < 10 ? '0' : '')
+    this.ret += '' + secs
+  }
+
+  getTimer() {
+    this.fancyTimeFormat(this.timer)
+    return this.ret
+  }
+}
+
+class LinhaVaraPesca extends Obj {
+  velocidade = 1
+
+  collide(obj) {
+    if (
+      this.x < obj.x + obj.width &&
+      this.x + obj.width > obj.x &&
+      this.y < obj.y + obj.height &&
+      this.y + obj.height > obj.y
+    ) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  mudaTamanho(aumenta) {
+    if (aumenta) {
+      if (this.height >= 150) {
+        this.height -= 10 * this.velocidade
+        this.y += 1
+      }
+    } else {
+      if (this.height <= 700) {
+        this.height += 10 * this.velocidade
+
+        this.y -= 1
+      }
+    }
+  }
+}
+
+class Isca extends Obj {
+  // image = "Assets/minhoca.png"
+
+  collide(obj) {
+    if (
+      this.x < obj.x + obj.width &&
+      this.x + obj.width > obj.x &&
+      this.y < obj.y + obj.height &&
+      this.y + obj.height > obj.y
+    ) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+class Salmao extends Obj {
+  speed = 40
+  tipoObj = this.tiposPossiveisObjeto[0]
+  objetoNome = this.peixesPossiveis[0]
+
+  imgPescado = 'Assets/salmaoPescado.png'
+}
+
+class Tilapia extends Obj {
+  speed = 30
+  tipoObj = this.tiposPossiveisObjeto[0]
+  objetoNome = this.peixesPossiveis[1]
+
+  imgPescado = 'Assets/tilapiaPescado.png'
+}
+
+class Pacu extends Obj {
+  speed = 40
+  tipoObj = this.tiposPossiveisObjeto[0]
+  objetoNome = this.peixesPossiveis[2]
+
+  imgPescado = 'Assets/pacuPescado.png'
+}
+
+class Bota extends Obj {
+  speed = 40
+  tipoObj = this.tiposPossiveisObjeto[1]
+  objetoNome = this.lixosPossiveis[0]
+
+  imgPescado = 'Assets/bota.png'
+}
+
+class Tubarao extends Obj {
+  speed = 40
+  tipoObj = this.tiposPossiveisObjeto[2]
+  objetoNome = this.inimigosPossiveis[0]
+}
+
+class Caranguejo extends Obj {
+  speed = 40
+  tipoObj = this.tiposPossiveisObjeto[2]
+  objetoNome = this.inimigosPossiveis[1]
+}
+
+class lataMinhoca extends Obj {
+  speed = 40
+  tipoObj = this.tiposPossiveisObjeto[3]
+  objetoNome = this.minhocasPossiveis[0]
+
+  imgPescado = 'Assets/lataMinhoca.png'
+}
+
+class invocarPeixe {
+  generateRandomIntegerInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  peixesVivos = []
+  larguraInicial = -600
+  alturaInicial = this.generateRandomIntegerInRange(300, 670)
+  peixeMomento
+
+  //Variavel
+  tubarao = false
+
+  gerarPeixe() {
+    //Posição inicial do peixe
+    this.larguraInicial = -600
+    this.alturaInicial = this.generateRandomIntegerInRange(300, 670)
+
+    var qualPeixe = this.generateRandomIntegerInRange(0, 3)
+    this.peixeMomento = this.instanciaPeixe(qualPeixe)
+    this.peixesVivos.push(this.peixeMomento)
+
+    //SEM INIMIGO
+    if (this.generateRandomIntegerInRange(0, 1) == 1) {
+      //Atras
+      if (this.generateRandomIntegerInRange(0, 1) == 0) {
+        this.larguraInicial -= 230
+      } else {
+        this.larguraInicial += 230
+      }
+      var qualInimigo = this.generateRandomIntegerInRange(4, 6)
+      this.peixeMomento = this.instanciaPeixe(qualInimigo)
+      this.peixesVivos.push(this.peixeMomento)
+    }
+  }
+
+  instanciaPeixe(index) {
+    switch (index) {
+      case 0:
+        return new Salmao(
+          this.larguraInicial,
+          this.alturaInicial,
+          142,
+          50,
+          3,
+          'Assets/salmao.png',
+        )
+      case 1:
+        return new Pacu(
+          this.larguraInicial,
+          this.alturaInicial,
+          142,
+          50,
+          3,
+          'Assets/pacu.png',
+        )
+      case 2:
+        return new Tilapia(
+          this.larguraInicial,
+          this.alturaInicial,
+          142,
+          50,
+          3,
+          'Assets/tilapia.png',
+        )
+      case 3:
+        return new lataMinhoca(
+          this.larguraInicial,
+          this.alturaInicial,
+          50,
+          99,
+          3,
+          'Assets/lataMinhoca.png',
+        )
+      case 4:
+        return new Caranguejo(
+          this.larguraInicial,
+          260,
+          129,
+          50,
+          1,
+          'Assets/caranguejo.png',
+        )
+      case 5:
+        return new Bota(
+          this.larguraInicial,
+          this.alturaInicial,
+          75,
+          81,
+          3,
+          'Assets/bota.png',
+        )
+      case 6:
+        return new Tubarao(
+          this.larguraInicial,
+          this.alturaInicial,
+          201,
+          90,
+          3,
+          'Assets/tubarao.png',
+        )
+    }
+  }
+
+  peixeFuncao() {
+    // Para manipular cada peixe no array que foi preenchido pela função invocarPeixe.
+    this.peixesVivos.forEach((peixin) => {
+      // console.log(peixin)
+      // Verfica se o peixe já foi coletado ou não. Caso não ele desenhará e chamará a função movimentação dos peixes que ainda não foram coletados, escolhi assim pois não achei como destroir um objeto depois de ser pego, então os que forem pegos irão passar por esse if e não serão desenhados e nem se movimentaram
+      if (!peixin.coletado) {
+        // Caso o peixe ainda não tenha sido fisgado ele entrará nesse if, que o desenhará, movimentará, e terá o collider caso encoste na isca.
+        if (peixin.pescado == false) {
+          peixin.desenha()
+          peixin.movement()
+
+          // Caso a isca colida com o peixe ele verificará se a isca já não está segurando um peixe, caso não ele entrará num switch para definir qual peixe foi pescado
+          if (iscaObj.collide(peixin)) {
+            if (!iscaObj.pescado) {
+              iscandoPeixe.play()
+              switch (peixin.objetoNome) {
+                case 'Pacu':
+                  iscaObj.pescado = true
+                  peixin.pescado = true
+                  console.log('Coletou Pacu')
+                  break
+                case 'Salmao':
+                  iscaObj.pescado = true
+                  peixin.pescado = true
+                  console.log('Coletou Salmao')
+                  break
+                case 'Tilapia':
+                  iscaObj.pescado = true
+                  peixin.pescado = true
+                  console.log('Coletou Tilapia')
+                  break
+                case 'Bota':
+                  iscaObj.pescado = true
+                  peixin.pescado = true
+                  console.log('Pegou bota')
+                  break
+                case 'Tubarao':
+                  if (player.tempoTubarao <= 0) {
+                    player.tempoTubarao = 5
+                    iscaObj.pescado = false
+                    player.iscas -= 1
+                    linha.height = 150
+                    linha.y = 10
+                    console.log('Tubarao comeu isca')
+                  }
+                  console.log('Tubarao comeu isca')
+                  break
+                case 'Lata':
+                  iscaObj.pescado = true
+                  peixin.pescado = true
+                  console.log('Pegou lata de minhocas')
+              }
             }
 
-        
-    }
-    
-
-    mudaTamanho(aumenta){
-        if(aumenta){
-            if(this.height >= 150){
-                
-                this.height -= 10 * this.velocidade
-                this.y += 1
+            // Caso ele tenha fisgado um peixe já, porém encostar num tubarão ele perderá o peixe e a isca
+            else {
+              if (peixin.objetoNome == 'Tubarao') {
+                if (player.tempoTubarao <= 0) {
+                  player.tempoTubarao = 5
+                  iscaObj.pescado = false
+                  player.iscas -= 1
+                  linha.height = 150
+                  linha.y = 10
+                  console.log('Tubarao comeu isca')
+                }
+              }
             }
-        }else{
-            if(this.height <= 700){
-                this.height += 10 * this.velocidade
-                
-                this.y -= 1
-            } 
+          }
         }
-    }   
-}
 
-class Isca extends Obj{
-    // image = "Assets/minhoca.png"
+        //Caso o peixe esteja fisgado. Ele irá arrumar o x dependendo do objeto.
+        else {
+          if (peixin.objetoNome == 'Lata') peixin.height = 60
+          if (peixin.objetoNome == 'Bota')
+            peixin.peixePescado(iscaObj.x - 25, iscaObj.y, peixin.imgPescado)
+          else peixin.peixePescado(iscaObj.x, iscaObj.y, peixin.imgPescado)
 
-    
-
-    collide(obj) {
-        if (this.x < obj.x + obj.width &&
-            this.x + obj.width > obj.x &&
-            this.y < obj.y + obj.height &&
-            this.y + obj.height > obj.y
-        ) {
-            return true;
-        } else {
-            return false;
+          // Caso a linha chegue na altura 150 quando tem um peixe fisgado ela entrará nesse if, que verificará qual objeto que foi coletado e tratará.
+          if (linha.height <= 150) {
+            if (peixin.objetoNome == 'Bota') {
+              player.iscas -= 1
+              iscaObj.pescado = false
+            }
+            if (peixin.objetoNome == 'Lata') {
+              player.iscas += 1
+              iscaObj.pescado = false
+              peixin.coletado = true
+            } else {
+              player.peixesPescados += 1
+              peixin.coletado = true
+              iscaObj.pescado = false
+              peixeColetado.play()
+            }
+          }
         }
-    }
+      }
+
+      if (peixin.objetoNome == 'Caranguejo') {
+        if (linha.height > 255 && peixin.x == 550) {
+          if (player.tempoTubarao <= 0) {
+            player.tempoTubarao = 5
+            player.iscas -= 1
+            linha.height = 150
+            linha.y = 10
+            iscaObj.pescado = false
+            console.log('caranguejo comeu isca')
+          }
+        }
+      }
+    })
+  }
 }
-
-class Salmao extends Obj{
-    speed = 40;
-    tipoObj = this.tiposPossiveisObjeto[0]
-    objetoNome = this.peixesPossiveis[0]
-
-    imgPescado = "Assets/salmaoPescado.png"
-
-}
-
-class Tilapia extends Obj{
-    speed = 30;
-    tipoObj = this.tiposPossiveisObjeto[0]
-    objetoNome = this.peixesPossiveis[1]
-
-    imgPescado = "Assets/tilapiaPescado.png"
-}
-
-class Pacu extends Obj{
-    speed = 40;
-    tipoObj = this.tiposPossiveisObjeto[0]
-    objetoNome = this.peixesPossiveis[2]
-
-    imgPescado = "Assets/pacuPescado.png"
-}
-
-class Bota extends Obj{
-    speed = 40;
-    tipoObj = this.tiposPossiveisObjeto[1]
-    objetoNome = this.lixosPossiveis[0]
-
-    imgPescado = "Assets/bota.png"
-}
-
-class Tubarao extends Obj{
-    speed = 40;
-    tipoObj = this.tiposPossiveisObjeto[2]
-    objetoNome = this.inimigosPossiveis[0]
-}
-
-class Caranguejo extends Obj{
-    speed = 40;
-    tipoObj = this.tiposPossiveisObjeto[2]
-    objetoNome = this.inimigosPossiveis[1]
-}
-
-class lataMinhoca extends Obj{
-    speed = 40;
-    tipoObj = this.tiposPossiveisObjeto[3]
-    objetoNome = this.minhocasPossiveis[0]
-
-    imgPescado = "Assets/lataMinhoca.png"
-}
-
--- Scripts/game.js
+++ Scripts/game.js
@@ -33,15 +33,23 @@ var telaMenu = new Obj(0, 0, 1200, 720, 0, 'Assets/menu.png')
 var minhocaIcon = new Img(0, 0, 120, 120, 'Assets/iconminhoca.png')
 
 //VARIAVEIS DO JOGO
+var rodandoJogo = false
 var gameOver = false
 const peixesVivos = []
 const caranguejoArray = []
 
+var gerarPeixe = new invocarPeixe()
+
+
+//Buttons
+//Menu inicial
+var jogarButton = new Button(940, 615, 205, 57, 'Assets/empty.png')
+
 
 //PEGAR O MOV DO MOUSE E MOVER A LARGURA DA LINHA COM ISSO E MOVER ISCA
 document.addEventListener('mousemove', (event) => {
-  var rect = canvas.getBoundingClientRect();
-  var y = event.clientY - rect.top;
+  var x = event.offsetX;
+  var y = event.offsetY;
 
   if(linha.height >= 150 && linha.height <= 700){
     linha.height = y
@@ -51,6 +59,21 @@ document.addEventListener('mousemove', (event) => {
     linha.height = 700
   }
   iscaObj.y = linha.y + linha.height - 20
+});
+
+document.addEventListener('click', (event) => {
+  var x = event.offsetX;
+  var y = event.offsetY;
+
+  if(!rodandoJogo && !gameOver){
+      //MENU
+      if(jogarButton.clickButton(x, y)){
+          player.timer = 300
+          player.tempoTubarao = 5
+          gerarPeixe.gerarPeixe()
+          rodandoJogo = true;
+      }
+  }
 });
 
 
@@ -77,307 +100,37 @@ function hud(){
   pincel.font = "60px Arial";
   pincel.fillText(player.iscas, 100, 80);
 
-  pincel.fillText(fancyTimeFormat(player.timer), 500, 80);
+  pincel.fillText(player.getTimer(), 500, 80);
 }
-
-
 
 
 //timer
 function timerContando() {
     player.timer -= 1
+    player.tempoTubarao -= 1
 }
 
-
-//FUNCAO SETAR FORMATO DO TIMER BUNITINHO 
-function fancyTimeFormat(duration)
-{   
-    // Hours, minutes and seconds
-    var hrs = ~~(duration / 3600);
-    var mins = ~~((duration % 3600) / 60);
-    var secs = ~~duration % 60; 
-
-    // Output like "1:01" or "4:03:59" or "123:03:59"
-    var ret = "";
-
-    if (hrs > 0) {
-        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
-    }
-
-    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
-    ret += "" + secs;
-    return ret;
-}
-
-//FUNCAO GERAR NUMERO INTEIRO RANDOMICO
-function generateRandomIntegerInRange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-
-//Criar peixe randomico, ela passará seus atributos para qa função invocarPeixe
-function criarPeixe() {
-
-  //Definir qual peixe vai ser
-  var qualPeixe = Math.floor(Math.random() * 4)
-
-  //Posição inicial do peixe
-  var larguraInicial = -600
-  var alturaInicial = generateRandomIntegerInRange(300, 670)
-
-  //Variavel
-  var tubarao = false
-
-
-  // Vai ter inimigo? Se for igual a 0 não vai ter, 1 vai ter
-  //Caso tenha um inimigo
-  if (generateRandomIntegerInRange(0, 1) == 0) {
-    var larguraInimigo = larguraInicial
-    tubarao = false
-    
-    // Vai ser atrás ou na frente do peixe? Se for 0 vai ser atrás, else vai ser na frente do peixe
-    if (generateRandomIntegerInRange(0, 1) == 0)
-      larguraInimigo -= 230
-    else 
-      larguraInimigo += 230
-
-
-    // Se for 0 gera um tubarão, caso entre no else gera uma bota
-    if (generateRandomIntegerInRange(0, 1) == 0)
-        invocarPeixe(6, larguraInimigo, alturaInicial)
-    else
-        invocarPeixe(5, larguraInimigo, alturaInicial - 20)
-  }
-
-  // Com as varíaveis do peixe criado elas serão passadas para essa função que vai gerar o peixe
-  invocarPeixe(qualPeixe, larguraInicial, alturaInicial)
-}
-
-function criarCaranguejo(){
-    invocarPeixe(4, -200, 200)
-}
-
-
-//Essa função vai invocar o peixe com as varíaveis da função criarPeixe
-function invocarPeixe(index, larguraInicial, alturaInicial) {
-  //Aqui a varíavel dizendo qual peixe que foi o escolhido
-  var peixeMomento
-
-
-  // Um switch para qual peixe vai ser gerado. Quando é o peixe selecionado ele vai instanciar o peixe com as varíaveis da função criarPeixe
-  switch (index) {
-    case 0:
-      var salmao = new Salmao(
-        larguraInicial,
-        alturaInicial,
-        142,
-        50,
-        3,
-        'Assets/salmao.png',
-      )
-      peixeMomento = salmao
-      break
-    case 1:
-      var pacu = new Pacu(
-        larguraInicial,
-        alturaInicial,
-        142,
-        50,
-        3,
-        'Assets/pacu.png',
-      )
-      peixeMomento = pacu
-      break
-    case 2:
-      var tilapia = new Tilapia(
-        larguraInicial,
-        alturaInicial,
-        142,
-        50,
-        3,
-        'Assets/tilapia.png',
-      )
-      peixeMomento = tilapia
-      break
-    case 3:
-      var lataDeMinhoca = new lataMinhoca(
-        larguraInicial,
-        alturaInicial,
-        50,
-        99,
-        3,
-        'Assets/lataMinhoca.png',
-      )
-      peixeMomento = lataDeMinhoca
-      break
-    case 4:
-      var caranguejo = new Caranguejo(
-        larguraInicial,
-        260,
-        129,
-        50,
-        1,
-        'Assets/caranguejo.png',
-      )
-      peixeMomento = caranguejo
-      break
-    case 5:
-      var bota = new Bota(
-        larguraInicial,
-        alturaInicial,
-        75,
-        81,
-        3,
-        'Assets/bota.png',
-      )
-      peixeMomento = bota
-      break
-    case 6:
-      var tubarao = new Tubarao(
-        larguraInicial,
-        alturaInicial,
-        201,
-        90,
-        3,
-        'Assets/tubarao.png',
-      )
-      peixeMomento = tubarao
-      break
-  }
-
-  //Aqui vai colocar a instancia que acabou de ser criada num array que será utilizado para manipular todos os peixes ao mesmo tempo
-  peixesVivos.push(peixeMomento)
-}
-
-
-// Essa função irá manipular se o peixe foi fisgado, chegou no objetivo, se era um peixe ou uma bota
-function peixeFuncao() {
-
-  // Para manipular cada peixe no array que foi preenchido pela função invocarPeixe.
-  peixesVivos.forEach((peixin) => {
-
-    // Verfica se o peixe já foi coletado ou não. Caso não ele desenhará e chamará a função movimentação dos peixes que ainda não foram coletados, escolhi assim pois não achei como destroir um objeto depois de ser pego, então os que forem pegos irão passar por esse if e não serão desenhados e nem se movimentaram
-    if (!peixin.coletado) {
-
-      // Caso o peixe ainda não tenha sido fisgado ele entrará nesse if, que o desenhará, movimentará, e terá o collider caso encoste na isca.
-      if (peixin.pescado == false) {
-        peixin.desenha()
-        peixin.movement()
-
-
-        // Caso a isca colida com o peixe ele verificará se a isca já não está segurando um peixe, caso não ele entrará num switch para definir qual peixe foi pescado
-        if (iscaObj.collide(peixin)) {
-          if (!iscaObj.pescado)
-          {
-            iscandoPeixe.play()
-            switch (peixin.objetoNome) {
-              case 'Pacu':
-                iscaObj.pescado = true
-                peixin.pescado = true
-                console.log('Coletou Pacu')
-                break
-              case 'Salmao':
-                iscaObj.pescado = true
-                peixin.pescado = true
-                console.log('Coletou Salmao')
-                break
-              case 'Tilapia':
-                iscaObj.pescado = true
-                peixin.pescado = true
-                console.log('Coletou Tilapia')
-                break
-              case 'Bota':
-                iscaObj.pescado = true
-                peixin.pescado = true
-                console.log('Pegou bota')
-                break
-              case 'Tubarao':
-                player.iscas -= 1
-                linha.height = 150
-                linha.y = 10
-                console.log("Tubarao comeu isca")
-                break
-              case 'Lata':
-                iscaObj.pescado = true
-                peixin.pescado = true
-                console.log("Pegou lata de minhocas")
-            }
-          } 
-          
-          // Caso ele tenha fisgado um peixe já, porém encostar num tubarão ele perderá o peixe e a isca
-          else {
-            if(peixin.objetoNome == "Tubarao"){
-              player.iscas -= 1
-              linha.height = 150
-              linha.y = 10
-              iscaObj.pescado = false
-              console.log("Tubarao comeu isca")
-            }
-          }
-        
-        }
-      } 
-
-      //Caso o peixe esteja fisgado. Ele irá arrumar o x dependendo do objeto.
-      else {
-        if(peixin.objetoNome == "Lata")
-          peixin.height = 50
-        if(peixin.objetoNome == "Bota")
-          peixin.peixePescado(iscaObj.x-25, iscaObj.y, peixin.imgPescado)
-        else 
-          peixin.peixePescado(iscaObj.x, iscaObj.y, peixin.imgPescado)
-        
-        
-        // Caso a linha chegue na altura 150 quando tem um peixe fisgado ela entrará nesse if, que verificará qual objeto que foi coletado e tratará.
-        if (linha.height <= 150) {
-          if(peixin.objetoNome == "Bota")
-          {
-            player.iscas -= 1
-            iscaObj.pescado = false
-          }
-          if(peixin.objetoNome == "Lata")
-          {
-            player.iscas += 1
-            iscaObj.pescado = false
-            peixin.coletado = true
-            
-          }
-          else{
-            player.peixesPescados += 1
-            peixin.coletado = true
-            iscaObj.pescado = false
-            peixeColetado.play()
-          }
-        }
-      }
-    }
-    // if(linha.collide(peixin)){
-    //   console.log("Colidiu peixe")
-    //   if(peixin.objetoNome == "Caranguejo"){
-    //     player.iscas -= 1
-    //     linha.height = 150
-    //     iscaObj.pescado = false
-    //     console.log("Caranguejo cortou linha")
-    //   }
-    // }
-  })
-
+function getNovoPeixe() {
+  var peixeGerado = gerarPeixe.gerarPeixe()
+  peixesVivos.push(peixeGerado)
 }
 
 function main() {
-  var rodandoJogo = false
-  var gameOver = false
   if(rodandoJogo){
     pincel.clearRect(0, 0, 1200, 720)
     mapaDesenho()
-    peixeFuncao()
+    gerarPeixe.peixeFuncao()
+    console.log(linha.x)
   } else if(gameOver){
-
+    
   }else{
-      telaMenu.desenha();
+    telaMenu.desenha();
   }
+  
+  
+  
 }
-criarCaranguejo()
+// criarCaranguejo()S
+setInterval(getNovoPeixe, 2500)
 setInterval(timerContando, 1000)
-setInterval(criarPeixe, 2500)
 setInterval(main, 10)
